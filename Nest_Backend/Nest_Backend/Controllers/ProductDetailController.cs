429e5222a766b662e93680948626d528ec1cddc5
Aysu Memmedova <aysu.memmedova2270@uni.edu.az>
2022-06-04 23:05:18 +0400

    add

---
+++ Nest_Backend/Nest_Backend/Controllers/ProductDetailController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nest_Backend.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nest_Backend.Controllers
+{
+    public class ProductDetailController : Controller
+    {
+        readonly AppDbContext _context;
+        public ProductDetailController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index(int id)
+        {
+            return View(await _context.Products.FindAsync(id));
+        }
+    }
+}
